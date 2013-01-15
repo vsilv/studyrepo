8528f4086d8ac516f7e9ae9ee12a1398d5ec82e9
vsilv <vsilverlord@googlemail.com>
2013-01-15 20:58:57 +0100

    added programming files

---
+++ gen/Math/General.py
@@ -0,0 +1,109 @@
+# Contexts
+C_LINE      = -1
+C_PARENTH     = 0
+C_SUM       = 1
+C_MINUS     = 1.5
+C_PRODUCT   = 2
+C_DIVISOR   = 2.5
+C_POWER     = 3
+
+class MathException():
+    def __init__(self, data):
+        self.data = data
+       
+
+        
+class NoNumericalValueFound(MathException):
+    def repr(self):
+        return "No numerical value for variable " + self.data + " found."
+    
+class MathObject:
+    def __init__(self):
+        self.space = ParameterSpace()
+    def eval(vars):
+        pass
+    def str(self, context=0):
+        pass
+    def latex(self, context=0):
+        pass
+    def simplify(self):
+        pass
+    def dependsOn(self, var):
+        pass
+    def derivate(self, var):
+        pass
+
+      
+      
+class Number(MathObject):
+    def e():
+        return Number(math.e)
+    def pi():
+        return Number(math.pi)
+    
+    def __init__(self, number):
+        self.number = number
+    def eval(self, **vars):
+        return self.number
+    def str(self, context=C_PARENTH):
+        return str(self.number).replace(".", ",")
+    def latex(self, context=C_PARENTH):
+        return str(self.number).replace(".", ",")
+    
+class Function(MathObject):
+
+    def __init__(self, name, argumentSet, inner):
+        self.name  = name
+        self.inner = inner
+        self.argumentSet = argumentSet
+    
+    def eval(self, vars):
+        self.inner = vars
+    
+    def call(self, *arguments):
+        vars = dict(zip(self.argumentSet, arguments))
+        return self.inner.eval(**vars)
+        
+    def derivate():
+        pass
+    def str(self, context=C_LINE):
+        args = ""
+        if len(self.argumentSet) > 0:
+            args = self.argumentSet[0]
+            for arg in self.argumentSet[1:]:
+                args += ", " + arg
+        if context == C_LINE:
+            return self.name  + "(" + args + ") = " + self.inner.str()
+        else:
+            return self.name  + "(" + args + ") "
+        
+    def latex(self, context= C_LINE):
+        args = ""
+        if len(self.argumentSet) > 0:
+            args = self.argumentSet[0]
+            for arg in self.argumentSet[1:]:
+                args += ", " + arg
+        if context == C_LINE:
+            return "\\textrm{" + self.name + "}" + "(" + args + ") = " + self.inner.latex()      
+        else:
+            return "\\textrm{" + self.name + "}" + "(" + args + ")" 
+      
+      
+    def derivate(self, var):
+        return self.inner.derivate(var)
+
+class Variable(MathObject):
+    def __init__(self, name):
+        self.name = name
+    def eval(self, **vars):
+        if self.name in vars.keys():
+            return vars[self.name]
+        else:
+            raise NoNumericalValueFound(self.name)
+    def str(self, context=C_PARENTH):
+        return self.name
+    def latex(self, context=C_PARENTH):
+        return self.name
+    
+    def dependsOn(self, var):
+        return  var == self.name
---
+++ gen/load_csv/ocd.py
@@ -0,0 +1,23 @@
+#usage: 
+# x = open_csv("table1.csv")
+# now all the variables are stored in x
+import csv 
+import sympy as sy
+from errorclass import *
+def open_csv(name):
+        class Bunch(object):
+          def __init__(self, adict):
+            self.__dict__.update(adict)
+        data_={}
+        with open(name,"rb") as csvfile:
+                data= csv.reader(csvfile, delimiter=",", quotechar="|")
+                names=data.next()
+                for k in names:
+                        data_[k]=[]
+                for row in data:
+                        for k in xrange(len(row)):
+                                data_[names[k]]+=[row[k]]
+        return Bunch(data_)
+def open_csv_errors(name):
+
+                                
